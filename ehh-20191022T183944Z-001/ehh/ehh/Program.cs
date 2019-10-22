9b90e5e929a1dbf14c864a0bb16abc4b1c76c117
Arthasior01 <44493014+Arthasior01@users.noreply.github.com>
2019-10-22 21:19:23 +0200

    Add files via upload

---
+++ ehh-20191022T183944Z-001/ehh/ehh/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ehh
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car c1 = new Car(10, "BMW");
+            c1.wypisz();
+            Console.WriteLine(Car.iloscKol);
+            //Console.WriteLine("Podaj pojemnosc silnika i marke");
+            //Car c2 = Car.Create();
+            //c2.wypisz();
+            //Console.WriteLine("Podaj pojemnosc silnika i marke");
+            //Car c3 = Car.Create();
+            //c3.wypisz();
+            Car c2 = new Car(5, "Smart");
+            c2 = c1;
+        }
+
+    }
+     public class Car
+    {
+        private double pojemnoscSilnika;
+        private string marka;
+        public static int iloscKol;
+        
+        public Car(double pojemnoscSilnika, string marka)
+        {
+            this.pojemnoscSilnika = pojemnoscSilnika;
+            this.marka = marka;
+        }
+        static Car()
+        {
+            iloscKol = 4;
+        }
+        public void wypisz()
+        {
+            Console.WriteLine("{0}  {1}", marka, pojemnoscSilnika);
+        }
+        public static Car Create()
+        {
+            return new Car(Convert.ToDouble(Console.ReadLine()), Convert.ToString(Console.ReadLine()));
+        }
+        ~Car()
+        {
+            Console.WriteLine("Zwalniam pamiec");
+            Console.ReadKey();
+        }
+    }
+}
